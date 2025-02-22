6471aa4f613fe08d0fadc62bfaf52bb09f45195d
thblonde <167370762+thblonde@users.noreply.github.com>
2025-02-22 15:14:57 +0100

    add keycode

--- include/cub3d.h
+++ include/cub3d.h
@@ -10,6 +10,23 @@
 # define WIDTH 500
 # define HEIGHT 500
 
+# define PI 3.14159265359
+
+// Linux
+# define W 119 
+# define S 115
+# define A 97
+# define D 100
+# define LEFT_ARROW 65361
+# define RIGHT_ARROW 65363
+
+// Macos
+//# define W 13
+//# define S 1
+//# define A 0
+//# define D 2
+//# define ESC 53
+
 typedef struct s_game
 {
 	void	*mlx;
